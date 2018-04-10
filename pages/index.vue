15d443668cdd706c5be237c3720b951144ca550e
shwld <shu.account@outlook.com>
2018-04-10 22:56:31 +0900

    借りるボタン追加

--- pages/index.vue
+++ pages/index.vue
@@ -7,13 +7,28 @@ v-container(fill-height grid-list-lg)
         v-card-title(:primary-title="item.title")
           div
             v-avatar(size="36").mr-2
-              img(src="https://randomuser.me/api/portraits/men/88.jpg" alt="")
+              img(src="https://randomuser.me/api/portraits/men/88.jpg" alt)
             span(v-text="'テキスト 太郎'")
           div.mt-3
             h3.headline.mb-0 {{item.title}}
             //- div {{item.description}}
         v-card-actions
-          v-btn(flat="" color="green") 借りる
+          v-btn(flat color="green" @click.native.stop="dialog = true") 借りる
+    v-dialog(v-model="dialog" persistent max-width="500px")
+      v-card
+        v-card-title
+          span.headline 借りる
+        v-card-text
+          v-container(grid-list-md)
+            v-layout(wrap)
+              v-flex(xs12)
+                v-text-field(label="アドレス" required)
+          small *indicates required field
+        v-card-actions
+          v-spacer
+          v-btn(color="blue darken-1" flat @click.native="dialog = false") Close
+          v-btn(color="blue darken-1" flat @click.native="dialog = false") Save
+
 </template>
 
 <script lang="ts">
@@ -26,6 +41,7 @@ import { Action, Getter } from 'vuex-class'
 export default class extends Vue {
   @Action('spaceList/load') loadSpaces
   @Getter('spaceList/items') items
+  dialog = false
 
   created() {
     this.loadSpaces()
